5dd9b0c365826b1a30c9df2ba765b9c06c93b634
Shruti Manohar Tanpure <62892396+shruti-tanpure@users.noreply.github.com>
2025-12-17 17:43:40 -0600

    Implement adjacency list representation of a graph

---
+++ GraphAdjList.java
@@ -0,0 +1,44 @@
+import java.util.List;
+import java.util.ArrayList;
+class GraphAdjList {
+    private int V; // number of vertices
+    private List<List<Integer>> adjList;
+
+    public GraphAdjList(int V) {
+        this.V = V;
+        adjList = new ArrayList<>();
+
+        for (int i = 0; i < V; i++) {
+            adjList.add(new ArrayList<>());
+        }
+    }
+
+    public void addEdge(int u, int v) {
+        adjList.get(u).add(v);
+        adjList.get(v).add(u); // remove for directed graph
+    }
+
+    public void printGraph() {
+        for (int i = 0; i < V; i++) {
+            System.out.print(i + " -> ");
+            for (int neighbor : adjList.get(i)) {
+                System.out.print(neighbor + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        GraphAdjList graph = new GraphAdjList(5);
+
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 4);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 4);
+
+        graph.printGraph();
+    }
+}
